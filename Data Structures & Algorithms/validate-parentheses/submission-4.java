a07bc4a55b7219ba0afc6a54112d6a265c602f29
Kshitiz-err <kshitijpimpale108@gmail.com>
2026-08-05 19:57:16 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack();
+        int n=s.length();
+        //if(n<=1)return false;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+                if(s.charAt(i)=='(')stack.push(')');
+                if(s.charAt(i)=='{')stack.push('}');
+                if(s.charAt(i)=='[')stack.push(']');
+                
+            }else{
+                if(stack.isEmpty())return false;
+                if(!stack.isEmpty())if(s.charAt(i)!=stack.pop())return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
